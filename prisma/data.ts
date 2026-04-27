612f606f9937627afcd3d21c3c0a53fab280e14c
Михаил Романов <mihailromanov798@gmail.com>
2026-04-27 20:07:34 +0600

    Подготовлены данные для сидирования ./prisma/data.ts, сделана логика сидировния ./prisma/seed.ts. (дополнительно в shema.prisma в сущности messege добавлено атрибут updateAt для логики обновления сообщения)

--- prisma/data.ts
+++ prisma/data.ts
@@ -1,35 +1,253 @@
-import {BracketType, MatchStatus, Role, TiebreakType, TypeStage } from "@prisma/client";
+import { RoleSystem, ChatType, ChatRole } from "@prisma/client";
 
 //====================== USER ==================
 //Таблица User 
 export const users = [
     {
         id: "user1",
-        firstname: "Юзер1",
-        surname: "Юзеров1",
-        email: "user1@err.com",
-        role: Role.USER,
+        firstname: "Олег",
+        surname: "Сахаров",
+        patronymic: "Григорьевич",
+        email: "oleg@megalule.com",
+        createdAt: new Date('2026-04-27T15:30:00'),
+        role: RoleSystem.USER,
     },
-]
-//Таблица User (админы)
-export const admins = [
+    {
+        id: "user2",
+        firstname: "Глеб",
+        surname: "Громов",
+        patronymic: "Алекберович",
+        email: "gromov@megalule.com",
+        createdAt: new Date('2026-04-27T15:30:00'),
+        role: RoleSystem.USER,
+    },
+    {
+        id: "user3",
+        firstname: "Семен",
+        surname: "Петров",
+        patronymic: "Валерьевич",
+        email: "petrov@megalule.com",
+        createdAt: new Date('2026-04-27T15:30:00'),
+        role: RoleSystem.USER,
+    },
+    {
+        id: "user4",
+        firstname: "Валерий",
+        surname: "Смальков",
+        patronymic: "Петрович",
+        email: "smalkov@megalule.com",
+        createdAt: new Date('2026-04-27T19:30:00'),
+        role: RoleSystem.USER,
+    },
     {
         id: "admin1",
-        firstname: "Админ1",
-        surname: "Админов1",
-        email: "admin1@err.com",
-        role: Role.ADMIN,
-    },
-]
-//Таблица User (организатор)
-export const organizer = [
-    {
-        id: "organizer1",
-        firstname: "Организатор1",
-        surname: "Организаторов1",
-        email: "organizer1@err.com",
-        role: Role.ORGANIZER,
+        firstname: "Костя",
+        surname: "Шмелев",
+        patronymic: "Генадьевич",
+        email: "shmelev@megalule.com",
+        createdAt: new Date('2025-04-22T18:30:00'),
+        role: RoleSystem.ADMIN,
     },
 ]
 
-//====================== НАЗВАНИЕ ==================
+//====================== CHATMEMBER ==================
+export const chatMember = [
+    //Пользователи чата 1 (личный чат)
+    {
+        id: "chatMember1",
+        userId: "user1",
+        chatId: "chat1",
+        role: ChatRole.USER,
+        joinedAt: new Date('2026-04-27T15:30:00'),
+        lastReadAt: new Date('2026-04-27T18:30:00'),
+    },
+    {
+        id: "chatMember2",
+        userId: "user2",
+        chatId: "chat1",
+        role: ChatRole.USER,
+        joinedAt: new Date('2026-04-27T15:30:00'),
+        lastReadAt: new Date('2026-04-27T19:30:00'),
+    },
+
+    //Пользователи чата 2 (групповой чат)
+    {
+        id: "chatMember3",
+        userId: "user1",
+        chatId: "chat2",
+        role: ChatRole.ADMIN,
+        joinedAt: new Date('2026-04-27T15:30:00'),
+        lastReadAt: new Date('2026-04-27T18:30:00'),
+    },
+    {
+        id: "chatMember4",
+        userId: "user2",
+        chatId: "chat2",
+        role: ChatRole.USER,
+        joinedAt: new Date('2026-04-27T15:30:00'),
+        lastReadAt: new Date('2026-04-27T19:36:00'),
+    },
+    {
+        id: "chatMember5",
+        userId: "user3",
+        chatId: "chat2",
+        role: ChatRole.USER,
+        joinedAt: new Date('2026-04-27T15:30:00'),
+        lastReadAt: new Date('2026-04-27T19:55:00'),
+    },
+    
+    //Пользователи чата 3 (канал чат)
+    {
+        id: "chatMember6",
+        userId: "user1",
+        chatId: "chat3",
+        role: ChatRole.ADMIN,
+        joinedAt: new Date('2026-04-27T15:30:00'),
+        lastReadAt: new Date('2026-04-27T18:30:00'),
+    },
+    {
+        id: "chatMember7",
+        userId: "user2",
+        chatId: "chat3",
+        role: ChatRole.USER,
+        joinedAt: new Date('2026-04-27T15:30:00'),
+        lastReadAt: new Date('2026-04-27T19:36:00'),
+    },
+    {
+        id: "chatMember8",
+        userId: "user3",
+        chatId: "chat3",
+        role: ChatRole.USER,
+        joinedAt: new Date('2026-04-27T15:30:00'),
+        lastReadAt: new Date('2026-04-27T19:55:00'),
+    },
+]
+
+//====================== CHAT ==================
+export const chats = [
+    {
+        id: "chat1",
+        chatType: ChatType.DIRECT,
+        createdAt: new Date(),
+    },
+    {
+        id: "chat2",
+        name: "Бухгалтерия",
+        description: "Отдел бухгалтерии, обсуждение финансов",
+        chatType: ChatType.GROUP,
+        createdAt: new Date(),
+    },
+    {
+        id: "chat3",
+        name: "Информация бухгалтерии",
+        description: "Основная информаци отдела бухгалтерии",
+        chatType: ChatType.CHANNEL,
+        createdAt: new Date(),
+    },
+]
+
+//====================== MASSAGE ==================
+export const massage = [
+    //Сообщения чата 1 (личный сообщения)
+    {
+        id: "massage1",
+        text: "Привет, теперь общение будет тут",
+        createdAt: new Date('2026-04-27T15:33:00'),
+        senderId: "user1",
+        chatId: "chat1",
+    },
+    {
+        id: "massage2",
+        text: "Привет, ясно",
+        createdAt: new Date('2026-04-27T15:37:00'),
+        senderId: "user2",
+        chatId: "chat1",
+    },
+    //Сообщения чата 2 (сообщения группы)
+    {
+        id: "massage3",
+        text: "Привет, теперь общение будет тут",
+        createdAt: new Date('2026-04-27T15:33:00'),
+        senderId: "user1",
+        chatId: "chat2",
+    },
+    {
+        id: "massage4",
+        text: "Привет, ясно",
+        createdAt: new Date('2026-04-27T15:37:00'),
+        senderId: "user2",
+        chatId: "chat2",
+    },
+    {
+        id: "massage5",
+        text: "Привет, Понятно",
+        createdAt: new Date('2026-04-27T15:38:00'),
+        senderId: "user3",
+        chatId: "chat2",
+    },
+    //Сообщения чата 3 (сообщения канала)
+    {
+        id: "massage6",
+        text: "Сегодня всем нужно быть в конференц зале",
+        createdAt: new Date('2026-04-28T15:33:00'),
+        senderId: "user1",
+        chatId: "chat2",
+    },
+]
+
+//====================== MASSAGEREAD ==================
+export const massageRead = [
+    //Сообщения чата 3 (канал)
+    {
+        id: "massageRead1",
+        messageId: "massage6",
+        userId: "user1",
+        readAt: new Date('2026-04-28T15:33:00'),
+    },
+    {
+        id: "massageRead2",
+        messageId: "massage6",
+        userId: "user2",
+        readAt: new Date('2026-04-28T16:00:00'),
+    },
+    {
+        id: "massageRead3",
+        messageId: "massage6",
+        userId: "user3",
+        readAt: new Date('2026-04-28T17:00:00'),
+    },
+]
+
+//====================== FOLDERCHAT ==================
+export const folderChat = [
+    {
+        id: "folderChat1",
+        chatId: "chat2",
+        folderId: "folder1",
+    },
+    {
+        id: "folderChat2",
+        chatId: "chat3",
+        folderId: "folder1",
+    },
+    {
+        id: "folderChat3",
+        chatId: "chat1",
+        folderId: "folder2",
+    },
+]
+
+//====================== FOLDER ==================
+export const folder = [
+    {
+        id: "folder1",
+        name: "Отдел бухгалтерии",
+        userId: "user1",
+    },
+    {
+        id: "folder2",
+        name: "Переписки с коллегами",
+        userId: "user1",
+    },
+]
+
--- prisma/seed.ts
+++ prisma/seed.ts
@@ -1,203 +1,128 @@
-import { PrismaClient, Role } from "@prisma/client";
+import { ChatType, PrismaClient,  } from "@prisma/client";
 import path from "path";
 import fs from "fs";
-import { admins, bracketMatches, bracketMatchResults, brackets, groupMatches, groupMatchResults, groups, organizer, participants, turnirParticipants, turnirs, users } from "./data";
+import { chatMember, chats, folder, folderChat, massage, massageRead, users } from "./data";
 
 const prisma = new PrismaClient();
 
-interface Arr{
-  id: string,
-  firstname: string,
-  surname: string,
-  email: string,
-  role: Role,
-}
-//Типовые функции
-async function User(arrUser: Arr[]){
-  for (const user of arrUser) {
-    await prisma.user.upsert({
-      where: { id: user.id },
-      update: {},
-      create: {
-        id: user.id,
-        firstname: user.firstname,
-        surname: user.surname,
-        email: user.email,
-        role: user.role,
-        emailVerified: new Date(),
-      },
-    });
-  }
-
-}
-
-//удаление всех данных из БД
+//функция удаления всех данных из БД
 async function clearDb() {
   console.log("Удаляем старые данные...");
-
-  await prisma.bracketMatchResult.deleteMany()
-  await prisma.bracketMatch.deleteMany()
-  await prisma.bracket.deleteMany()
-
-  await prisma.groupMatchResult.deleteMany()
-  await prisma.groupMatch.deleteMany()
-  await prisma.group.deleteMany()
-
-  await prisma.turnirParticipant.deleteMany()
-  await prisma.turnir.deleteMany()
-  await prisma.participant.deleteMany()
+  await prisma.folderChat.deleteMany()
+  await prisma.folder.deleteMany()
+  await prisma.messageRead.deleteMany()
+  await prisma.message.deleteMany()
+  await prisma.chatMember.deleteMany()
+  await prisma.chat.deleteMany()
   await prisma.user.deleteMany()
 
-  console.log("База очищена");
+  console.log("🗑️ База очищена");
 }
 
 async function main() {
-  // Удаляем данные
-  await clearDb()
+  await clearDb()     // Удаляем данные
 
   console.log("🌱 Сидирование фиксированных данных...");
 
 // =====  USER =====
-  // Добавление пользователей, админов, организаторов
-  await User(users)
-  await User(admins)
-  await User(organizer)
-  // await User(referee)
-
-
-// ===== PARTICIPANT =====
-  // Создание сезонов
-  for (const participant of participants) {
-    await prisma.participant.upsert({
-      where: { id: participant.id },
+   for (const user of users) {
+    await prisma.user.upsert({
+      where: { id: user.id },
       update: {},
       create: { 
-        id: participant.id, 
-        firstname: participant.firstname, 
-        surname: participant.surname,
-        rating: participant.rating
+        id: user.id, 
+        firstname: user.firstname, 
+        surname: user.surname,
+        patronymic: user.patronymic,
+        email: user.email,
+        createdAt: user.createdAt,
+        role: user.role,
+      },
+    })
+  } 
+
+//====================== CHAT ==================
+  for (const chat of chats) {
+    await prisma.chat.upsert({
+      where: { id: chat.id },
+      update: {},
+      create: {
+        id: chat.id,
+        name: chat?.name,
+        description: chat?.description,
+        chatType: chat.chatType,
+        createdAt: chat.createdAt,
+      },
+    });
+  }
+
+//=========== CHATMEMBER =====
+  for (const cm of chatMember) {
+    await prisma.chatMember.upsert({
+      where: { id: cm.id },
+      update: {},
+      create: { 
+        id: cm.id, 
+        userId: cm.userId,
+        chatId: cm.chatId,
+        role: cm.role,
+        joinedAt: cm.joinedAt,
+        lastReadAt: cm.lastReadAt,
       },
     })
   }
 
-// =========== TURNIR ====================
-  for (const turnir of turnirs) {
-    await prisma.turnir.upsert({
-      where: { id: turnir.id },
+//====================== MASSAGE ==================
+  for (const m of massage) {
+    await prisma.message.upsert({
+      where: { id: m.id },
       update: {},
       create: {
-        id: turnir.id,
-        nameTurnir: turnir.nameTurnir,
-        description: turnir.description,
-        stage: turnir.stage,
-        participantsCount: turnir.participantsCount,
-        groupsCount: turnir.groupsCount,
-        tiebreakType: turnir.tiebreakType,
-        createdById: turnir.createdById
+        id: m.id,
+        text: m.text,
+        createdAt: m.createdAt,
+        senderId: m.senderId,
+        chatId: m.chatId,
       },
     });
   }
 
-
-// ============== GROUP =================
-  for (const group of groups) {
-    await prisma.group.upsert({
-      where: { id: group.id },
+//====================== MASSAGEREAD ===============
+  for (const mr of massageRead) {
+    await prisma.messageRead.upsert({
+      where: { id: mr.id },
       update: {},
       create: {
-        id: group.id,
-        name: group.name,
-        tournamentId: group.tournamentId
+        id: mr.id,
+        messageId: mr.messageId,
+        userId: mr.userId,
+        readAt: mr.readAt,
       },
     });
   }
 
-// ============== TurnirParticipant =================
-  for (const tp of turnirParticipants) {
-    await prisma.turnirParticipant.upsert({
-      where: { id: tp.id },
+//====================== FOLDER ==================
+  for (const f of folder) {
+    await prisma.folder.upsert({
+      where: { id: f.id },
       update: {},
       create: {
-        id: tp.id,
-        participantId: tp.participantId,
-        tournamentId: tp.tournamentId,
-        groupId: tp.groupId,
-      },
-    });
-  }  
-
-// ============== groupMatches =================
-  for (const gm of groupMatches) {
-    await prisma.groupMatch.upsert({
-      where: { id: gm.id },
-      update: {},
-      create: {
-        id: gm.id,
-        round: gm.round,
-        playerAId: gm.playerAId,
-        playerBId: gm.playerBId,
-        status: gm.status,
-        groupId: gm.groupId,
+        id: f.id,
+        name: f.name,
+        userId: f.userId,
       },
     });
   }
 
-// ============== groupMatchResult =================
-  for (const gmr of groupMatchResults) {
-    await prisma.groupMatchResult.upsert({
-      where: { id: gmr.id },
+//====================== FOLDERCHAT ================
+  for (const fc of folderChat) {
+    await prisma.folderChat.upsert({
+      where: { id: fc.id },
       update: {},
       create: {
-        id: gmr.id,
-        scoreA: gmr.scoreA,
-        scoreB: gmr.scoreB,
-        winnerId: gmr.winnerId,
-        groupMatchId: gmr.groupMatchId,
-      },
-    });
-  }
-
-// ============== BRACKET =================
-  for (const bracket of brackets) {
-    await prisma.bracket.upsert({
-      where: { id: bracket.id },
-      update: {},
-      create: {
-        id: bracket.id,
-        type: bracket.type,
-        doubleElim: bracket.doubleElim,
-        tournamentId: bracket.tournamentId,
-      },
-    });
-  }
-
-// ============== BracketMatch =================
-  for (const bm of bracketMatches) {
-    await prisma.bracketMatch.upsert({
-      where: { id: bm.id },
-      update: {},
-      create: {
-        id: bm.id,
-        round: bm.round,
-        status: bm.status,
-        playerAId: bm.playerAId,
-        playerBId: bm.playerBId,
-        bracketId: bm.bracketId,
-      },
-    });
-  }
-
-// ============== BracketMatchResult =================
-  for (const bmr of bracketMatchResults) {
-    await prisma.bracketMatchResult.upsert({
-      where: { id: bmr.id },
-      update: {},
-      create: {
-        id: bmr.id,
-        scoreA: bmr.scoreA,
-        scoreB: bmr.scoreB,
-        winnerId: bmr.winnerId,
-        bracketMatchId: bmr.bracketMatchId,
+        id: fc.id,
+        chatId: fc.chatId,
+        folderId: fc.folderId,
       },
     });
   }
